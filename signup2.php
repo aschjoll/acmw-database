198fcbd0c67fcfadeea9c42885924477bd389c03
adriennecorwin <adriennecorwin@gmail.com>
2018-10-29 12:19:27 -0400

    sign in start pt2

---
+++ signup2.php
@@ -0,0 +1,28 @@
+
+<?php
+function startHtml(){
+	echo "<html>";
+	echo "<title>ACM-W Sign In</title>";
+	echo "<body>";
+}
+
+function endHTML(){
+    echo "</body>";
+    echo "</html>";
+}
+
+function displaySignUp(){
+	startHtml();
+	echo "<form action=\"index.php\" method=\"post\">";
+	
+	echo "<input type=\"submit\" name=\"register\" value=\"SIGN UP\">";	
+	endHtml();
+}
+
+function insertUser(){
+
+}
+
+
+
+?>
